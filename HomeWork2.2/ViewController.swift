271e88695a5be40c13eec436fe6fe679df8c5b27
HelloWorld <tanysik14>
2022-11-05 16:59:50 +0300

    setup interfeis

--- HomeWork2.2/ViewController.swift
+++ HomeWork2.2/ViewController.swift
@@ -15,11 +15,30 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-   
+        trafficLightColorRed.layer.cornerRadius = 30
+        trafficLightColorYellow.layer.cornerRadius = 30
+        trafficLightColorGreen.layer.cornerRadius = 30
+        buttonStart.layer.cornerRadius = 5
+        trafficLightColorRed.alpha = 0.3
+        trafficLightColorYellow.alpha = 0.3
+        trafficLightColorGreen.alpha = 0.3
     }
 
 
     @IBAction func trafficLightSwitching() {
+        trafficLightColorRed.alpha = 1
+        buttonStart.setTitle("Next" , for: .normal)
+      
+          if trafficLightColorRed.alpha == 1 {
+            trafficLightColorYellow.alpha = 1
+            trafficLightColorRed.alpha = 0.3
+        } else if trafficLightColorYellow.alpha == 1 {
+            trafficLightColorGreen.alpha = 1
+            trafficLightColorYellow.alpha = 0.3
+        } else if trafficLightColorGreen.alpha == 1 {
+            trafficLightColorRed.alpha = 1
+            trafficLightColorGreen.alpha = 0.3
+        }
     }
     
 }
